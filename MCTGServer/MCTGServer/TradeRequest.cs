8539d3f8627a2f20d4061eae97d9b334d3bd42a6
Armin Dervisefendic <armindervisefendic@gmail.com>
2025-01-06 23:34:04 +0100

    update

--- MCTGServer/MCTGServer/TradeRequest.cs
+++
@@ -1,7 +0,0 @@
-namespace MCTG
-{
-    public class TradeRequest
-    {
-        public string CardName { get; set; } = string.Empty;
-        public string Requirement { get; set; } = string.Empty;
-}
